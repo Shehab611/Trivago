3a824edd3fafea8d089bb7886ce03e44b7e92dd6
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2023-04-18 16:02:33 +0200

    .

--- WindowsFormsApp1/database_control.cs
+++ WindowsFormsApp1/database_control.cs
@@ -13,7 +13,7 @@ using System.Xml.Linq;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace WindowsFormsApp1
-{
+{//
     internal class Database_control
     {
         //static string ordb = @"Data source=localhost:1521/xe;User Id=scott;Password=tiger;";
