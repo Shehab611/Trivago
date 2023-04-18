b04002d2f4d2fd58cd67c911ae9f535773168669
Mohand Mahmoud <mohandmahmoud01124762559@gmail.com>
2023-04-18 11:51:24 +0200

    .

--- WindowsFormsApp1/Hotel_Form.cs
+++ WindowsFormsApp1/Hotel_Form.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Hotel_Form : Form()
+    public partial class Hotel_Form : Form
     {
         public Hotel_Form()
         {
